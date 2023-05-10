a044c7db70d9d79d1d530de7acefd5b92b126e7b
Enderna <adryanender@gmail.com>
2023-05-10 14:26:09 +0200

    Added styles for gallery

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,6 +1,7 @@
 import { View, Text, Image, FlatList, StyleSheet, Dimensions } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import * as MediaLibrary from "expo-media-library";
+import colors from '../data/colors.json'
 
 export default function Gallery() {
     const [photos, setPhotos] = useState([]);
@@ -14,7 +15,6 @@ export default function Gallery() {
                     first: 100,
                     mediaType: 'photo'
                 })
-                
                 setPhotos(obj.assets);
             }
         })()
@@ -27,7 +27,11 @@ export default function Gallery() {
             <FlatList 
                 data={photos}
                 numColumns={4}
-                renderItem={({item})=><Image source={{uri: item.uri, width: dims.width/4, height: dims.width/4}}/>}
+                renderItem={({item})=>
+                <View style={styles.imageView}>
+                    <Image style={styles.image} source={{uri: item.uri, width: dims.width/4.5, height: dims.width/4.5}}/>
+                    <Text style={styles.imageText}>{item.id}</Text>
+                </View>}
             />  
             :
             <Text>{photos.length}</Text>
@@ -41,5 +45,22 @@ const dims = Dimensions.get('screen');
 const styles = StyleSheet.create({
     container: {
       flex: 1
+    },
+    image:{
+        borderRadius: 20
+    },
+    imageView:{
+        width: dims.width/4,
+        height: dims.width/4,
+        justifyContent: 'center',
+        alignItems: 'center'
+        
+    },
+    imageText:{
+        position: 'absolute',
+        right: 10,
+        bottom: 10,
+        fontSize: 10,
+        color: colors.mainTextColor
     }
   });
