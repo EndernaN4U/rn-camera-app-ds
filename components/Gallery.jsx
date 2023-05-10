7af6ae405f9cf406d30efd667e8ebc9769ab1f24
Enderna <adryanender@gmail.com>
2023-05-10 14:13:03 +0200

    Working on gallery

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,10 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, FlatList, StyleSheet, Dimensions } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import * as MediaLibrary from "expo-media-library";
 
 export default function Gallery() {
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(()=>{
+        (async()=>{
+            let {status} = await MediaLibrary.requestPermissionsAsync();
+            if(status !== 'granted') alert('Brak uprawnien');
+            else{
+                let obj = await MediaLibrary.getAssetsAsync({
+                    first: 100,
+                    mediaType: 'photo'
+                })
+                
+                setPhotos(obj.assets);
+            }
+        })()
+    },[])
+
   return (
-    <View>
-      <Text>Gallery</Text>
+    <View style={styles.container}>
+        {
+            (photos.length > 0)?
+            <FlatList 
+                data={photos}
+                numColumns={4}
+                renderItem={({item})=><Image source={{uri: item.uri, width: dims.width/4, height: dims.width/4}}/>}
+            />  
+            :
+            <Text>{photos.length}</Text>
+        }
     </View>
   )
 }
+
+const dims = Dimensions.get('screen');
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1
+    }
+  });
