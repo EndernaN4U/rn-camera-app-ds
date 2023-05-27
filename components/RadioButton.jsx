9c15cfb406036bbbc3a7ce7c1fd3d76a8771c111
Enderna <adryanender@gmail.com>
2023-05-27 23:58:30 +0200

    Styling RadioButton

--- components/RadioButton.jsx
+++ components/RadioButton.jsx
@@ -1,8 +1,15 @@
-import { TouchableOpacity } from 'react-native'
+import { TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
 
-export default function RadioButton({value, isChecked, onPress}) {
+export default function RadioButton({value, isChecked, onPress, size}) {
+  const styles = StyleSheet.create({
+    width: size || 20,
+    height: size || 20,
+  })
   return (
-    <TouchableOpacity onPress={()=>onPress(value)} style={{width: 20, height: 20, backgroundColor: isChecked? "green": "red"}} />
+    <TouchableOpacity 
+      onPress={()=>onPress(value)}
+      style={{width: size || 20, height: 20, backgroundColor: isChecked? "green": "red"}}
+    />
   )
 }
